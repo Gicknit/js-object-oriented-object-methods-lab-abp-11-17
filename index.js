b393afa1299e9388dff6890801edd5662b3198ba
Nick Garcia <89nickgarcia@gmail.com>
2017-11-12 20:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ function BoardMember (name, homeState, training){
   this.veto = function(){
     return "No, I must disagree";
   }
+  this.approve = function(){
+    return "You can do that!"
+  }
+  this.doCharity = function(){
+    return "I like to help people."
+  }
 }
 
 let polishedBoardMember = BoardMember ('Mr.Polished', 'New York', 'Law')
 
-polishedBoardMember.Veto()
+polishedBoardMember.veto()
+polishedBoardMember.approve()
+polishedBoardMember.doCharity()
