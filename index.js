3f2054b83117ba4a6f03c005d489f0332e12ce64
Nick Garcia <89nickgarcia@gmail.com>
2017-11-12 20:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function BoardMember (name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
+  this.veto = function(){
+    console.log("No, I must disagree")
+  }
 }
 
 let polishedBoardMember = BoardMember ('Mr.Polished', 'New York', 'Law')
+
+polishedBoardMember.Veto()
