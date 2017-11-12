29badc6260c43f5f2bf7740e85ce71f408ffcb28
Nick Garcia <89nickgarcia@gmail.com>
2017-11-12 20:22:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function BoardMember (name, state, training){
+  this.name = name
+  this.state = state
+  this.training = training
+}
+
+let polishedBoardMember = BoardMember ('Mr.Polished', 'New York', 'Law')
